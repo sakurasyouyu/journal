9b1c5ba6389dc2b112f50e088a4a0c558c2bb2e9
SAKURAYU <sakurasyouyu.no.ki@gmail.com>
2026-04-10 23:34:07 +0900

    Inject Supabase connection credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ご自身で取得したSupabaseのURLとキーをここに入力するか、.envファイルを作成して読み込ませます
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xxx.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'xxx_key';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://nrscddlunaxkqinmrrjs.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yc2NkZGx1bmF4a3Fpbm1ycmpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjIwMzIsImV4cCI6MjA5MTM5ODAzMn0.5HbdYLacaXnjPg8vPpTY-kcjU_GZ-17J9RCKkQCRaP8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
